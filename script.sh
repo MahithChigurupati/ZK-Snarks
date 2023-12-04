632461b7c1b829888c255dd1e5b8de86ddd71420
Mahith Chigurupati <saimahith.chigurupati@gmail.com>
2023-12-04 18:42:57 -0500

    basic setup for zk snarks - circom

---
+++ script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Step 1: Run circom command
+circom multiplier2.circom --r1cs --wasm --sym --c
+
+# Step 2: Create input.json with arguments passed from script.js
+echo "{\"a\": \"$1\", \"b\": \"$2\"}" > ./multiplier2_js/input.json
+
+echo "test"
+cd multiplier2_js
+
+# Step 3: Run generate_witness.js
+node generate_witness.js multiplier2.wasm input.json witness.wtns
+
+cd ..
+
+# Step 4: Run snarkjs commands
+snarkjs powersoftau new bn128 12 pot12_0000.ptau -v
+snarkjs powersoftau contribute pot12_0000.ptau pot12_0001.ptau --name="First contribution" -v
+snarkjs powersoftau prepare phase2 pot12_0001.ptau pot12_final.ptau -v
+snarkjs groth16 setup multiplier2.r1cs pot12_final.ptau multiplier2_0000.zkey
+snarkjs zkey contribute multiplier2_0000.zkey multiplier2_0001.zkey --name="1st Contributor Name" -v
+snarkjs zkey export verificationkey multiplier2_0001.zkey verification_key.json
+snarkjs groth16 prove multiplier2_0001.zkey witness.wtns proof.json public.json
+snarkjs groth16 verify verification_key.json public.json proof.json
+snarkjs zkey export solidityverifier multiplier2_0001.zkey verifier.sol
+snarkjs generatecall
